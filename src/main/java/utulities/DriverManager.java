2ee9307f986d5dc0abf7f3d568d589f5425d625b
Ru <95thru@gmail.com>
2024-03-19 22:05:36 -0400

    new package added

--- src/main/java/utulities/DriverManager.java
+++ src/main/java/utulities/DriverManager.java
@@ -1,4 +1,35 @@
 package utulities;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
 public class DriverManager {
+    private static WebDriver driver;
+
+    // Метод для инициализации драйвера
+    public static void initializeDriver(String browser) {
+        if (browser.equalsIgnoreCase("chrome")) {
+            System.setProperty("webdriver.chrome.driver", "path/to/chromedriver");
+            driver = new ChromeDriver();
+        } else if (browser.equalsIgnoreCase("firefox")) {
+            System.setProperty("webdriver.gecko.driver", "path/to/geckodriver");
+            driver = new FirefoxDriver();
+        } else {
+            throw new IllegalArgumentException("Unsupported browser: " + browser);
+        }
+    }
+
+    // Метод для получения текущего экземпляра драйвера
+    public static WebDriver getDriver() {
+        return driver;
+    }
+
+    // Метод для завершения работы драйвера и освобождения ресурсов
+    public static void quitDriver() {
+        if (driver != null) {
+            driver.quit();
+            driver = null;
+        }
+    }
 }
