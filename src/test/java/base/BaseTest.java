654906369e93e523a1e7ed8724aaf6b40124fc87
05 <qatsby@yandex.com>
2024-09-12 22:17:28 -0400

    .

--- src/test/java/base/BaseTest.java
+++ src/test/java/base/BaseTest.java
@@ -6,6 +6,7 @@ import org.junit.Test;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import pages.MetroHomePage;
 
 import java.util.concurrent.TimeUnit;
 
@@ -15,8 +16,13 @@ protected WebDriver driver;
 @Before
     public void setUp() {
     driver = new ChromeDriver();
+    driver.get("https://qa-metro.stand-2.praktikum-services.ru/");
     //driver = new FirefoxDriver();
     driver.manage().timeouts().implicitlyWait(8, TimeUnit.SECONDS);
+    MetroHomePage metroPage = new MetroHomePage(driver);
+    // дождались загрузки страницы
+    metroPage.waitForLoadHomePage();
+
 }
 
 @After
